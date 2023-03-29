c99c5cfe2c61a17470c664d676a814c91a089233
Luis <luisjavier_2705@hotmail.com>
2023-03-29 18:11:35 -0500

    correccion

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -7,8 +7,8 @@ const Modal = ({ cancion,setcancion }) => {
     <>
       {
         cancion ?
-          <div className="bg-red-700 flex  justify-center items-center w-full fixed bottom-0 left-0 h-28 transition-all p-7 rounded-t-2xl ">
-            <audio  autoPlay className="md:w-3/5 w-full " src={cancion} controls ></audio >
+          <div className="bg-red-700 border-red-900 border-2 shadow-2xl flex justify-center items-center w-full fixed bottom-0 left-0 h-28 transition-all p-7  ">
+            <audio autoPlay className="md:w-3/5 w-full shadow-2xl " src={cancion} controls ></audio >
             <button className=" text-white absolute top-3 -scale-y-125 right-5" onClick={() => setcancion('')} ><FontAwesomeIcon icon={faXmark} /> </button>
           </div > :
           <div className='bg-red-700 flex justify-center items-center w-full  fixed bottom-0 left-0 h-28 translate-y-36 transition-all'>
