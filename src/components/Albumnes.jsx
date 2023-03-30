f25649f76823e28f044b99938a2f4f5716cfef69
Luis <luisjavier_2705@hotmail.com>
2023-03-29 20:13:23 -0500

    ultimos estilos

--- src/components/Albumnes.jsx
+++ src/components/Albumnes.jsx
@@ -5,8 +5,8 @@ const Albumnes = ({ albumDes,mostrarAudio }) => {
   return (
     <div className='mt-8 flex flex-col justify-center items-center gap-3'>
       <h1 className='font-bold text-center text-2xl font-serif'>{title}</h1>
-      <h2>{artist.name}</h2>
-      <img className='rounded-3xl shadow-2xl md:w-3/4 cursor-pointer' onClick={()=>mostrarAudio(preview,artist,title,album)} src={album.cover_big} alt={title} />
+      <h2 className='font-medium'>{artist.name}</h2>
+      <img className='rounded-3xl brightness-75 hover:brightness-125 transition-all shadow-2xl md:w-3/4 cursor-pointer' onClick={()=>mostrarAudio(preview,artist,title,album)} src={album.cover_big} alt={title} />
       {/* <audio className='w-full md:w-3/4 ' src={preview} controls></audio> */}
     </div>
   )
